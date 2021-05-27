96be431d13adbeb05e94855f33128efbbf96b29e
Nils Lieber <43197556+nilslieber@users.noreply.github.com>
2021-05-27 17:24:28 +0200

    Revert "Update del script para creación de tabla de Menores"
    This reverts commit 93a73ee85909fe724c2454fbc3a9ffd7165ddc2f.

--- Scripts/analisis.R
+++ Scripts/analisis.R
@@ -55,11 +55,11 @@ ggplot(data = df, aes(Departamento, RyM)) +
 ggsave(path = "Resultados/Figuras", filename =  "RyM.png")
 
 # Bar diagram - Asistencia
-ggplot(data = df, aes(Departamento, Menores)) + 
+ggplot(data = df, aes(Departamento, Asistencia)) + 
   geom_bar(stat= "identity")
 
 # Guardar Figura
-ggsave(path = "Resultados/Figuras", filename =  "Menores.png")
+ggsave(path = "Resultados/Figuras", filename =  "Asistencia.png")
 
 
 
